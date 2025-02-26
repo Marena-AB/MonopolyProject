6126fd85f17d471050175438ba09d1e1bb489a5a
washingtonr <washingtonr@moravian.edu>
2025-02-26 14:43:36 -0500

    Added a function 'userLandsOnCommunityChestSpot' to implement that logic for that class

--- src/CommunityChestSpot.java
+++ src/CommunityChestSpot.java
@@ -10,6 +10,14 @@ public class CommunityChestSpot {
         this.type = "Community Chest";
     }
 
+    public void userLandsOnCommunityChestSpot() {
+        // Logic for pulling a card from the Community Chest deck
+        // This could involve randomizing a card from a predefined list of cards
+        // which can result in either receiving money, paying money to the bank,
+        // or being instructed to move to a different space on the board
+
+    }
+
     @Override
     public String toString() {
         return position + ": " + name + " (" + type + ")";
