830e13dd12564054bd43ced70489c3b1277c3aee
Marena-AB <abboudm@moravian.edu>
2025-03-10 22:43:04 -0400

    Added Methods to Tokens.java

--- src/Tokens.java
+++ src/Tokens.java
@@ -1,2 +1,15 @@
 public class Tokens {
+
+    private String owner;
+    private String BoardPosition;
+
+
+
+    public Tokens(String owner, String BoardPosition) {
+        this.owner = owner;
+        this.BoardPosition = BoardPosition;
+    }
+
+
+
 }
